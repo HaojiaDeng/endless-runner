7c61d40ca82052284288e3d818341e6a95b97033
HaojiaDeng <Hdeng20@ucsc.edu>
2024-02-15 23:22:54 -0800

    comments

--- src/main.js
+++ src/main.js
@@ -17,3 +17,19 @@ let game = new Phaser.Game(config)
 
 let keyH, keyB, keyP, keyRight, keySpace, keyLeft, keyR
 
+
+`
+Name: Haojia Deng
+Title: RooftopRunner
+Hours of working: 19 hours
+In this assignment I implemented the handling of gravity as well as inertia,
+which gave my character a higher degree of flexibility. 
+In addition, I also implemented the ability to increase the difficulty over time.
+for example, the lightning in the game will keep appearing at an accelerated rate.
+And then the position of the pedals is also completely randomized.
+That two are technically interesting to me.
+In addition, I experimented with the camera shake effect
+to increase the immersion of the player`
+
+
+
